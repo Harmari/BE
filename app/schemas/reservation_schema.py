ca639655036c584a7824c80de83861e205c9a389
Ruah Kim <kimhk0315@gmail.com>
2025-02-10 21:59:27 +0900

    feat : create schema

---
+++ app/schemas/reservation_schema.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class Customer(BaseModel):
+    name: str
+    email: EmailStr
+    city: str
+
+class Designer(BaseModel):
+    name: str
+    salon: str
+
+class ReservationDetails(BaseModel):
+    date: str
+    time: str
+    type: str
+    price: int
+
+class Payment(BaseModel):
+    method: str
+    status: str
+
+class Reservation(BaseModel):
+    customer: Customer
+    designer: Designer
+    reservation: ReservationDetails
+    payment: Payment
+
+class ReservationQuery(BaseModel):
+    email: EmailStr
