7a7780bc61f7875b91099f8b91910c63d79c3dce
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-02-13 18:39:13 +0900

    feat: 로그인시 success를 통해 구분

--- app/api/auth.py
+++ app/api/auth.py
@@ -24,27 +24,41 @@ async def auth_callback(request: Request, response: Response):
         # 인증 코드 가져오기
         code = request.query_params.get("code")
         if not code:
-            raise HTTPException(status_code=400, detail="code를 가져오지 못하는중..")
+            raise HTTPException(status_code=400, detail={
+                "success": False,
+                "message": "로그인 실패",
+            })
 
         # 토큰 요청
         token_data = await get_google_access_token(code)
         access_token = token_data.get("access_token")
         if not access_token:
-            raise HTTPException(status_code=400, detail="access token 못가져오는 중..")
+            raise HTTPException(status_code=400, detail={
+                "success": False,
+                "message": "로그인 실패",
+            })
 
         # 사용자 정보 요청
         userinfo = await get_google_user_info(access_token)
+        if not userinfo or "email" not in userinfo:
+            raise HTTPException(status_code=400, detail={
+                "success": False,
+                "message": "로그인 실패",
+            })
 
         # 로그인 성공 시 쿠키 설정
         await authenticate_user(userinfo, response)
 
-        # React 프론트엔드로 리디렉트
-        return {"message": "로그인 성공"}
-    
+        return {"success": True, "message": "로그인 성공"}
+
     except HTTPException as e:
         raise e
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"error: {str(e)}")
+        raise HTTPException(status_code=500, detail={
+            "success": False,
+            "message": "로그인 실패.",
+        })
+
 
 @router.post("/refresh")
 async def refresh_token(request: Request, response: Response):
